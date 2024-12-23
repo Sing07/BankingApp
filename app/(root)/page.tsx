1975ba001887cef4cdccb584f68aa0ddb58a9e9b
SingT <61646543+Sing07@users.noreply.github.com>
2024-12-22 18:30:15 -0800

    Header Box update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    const loggedIn = { firstName: "Sing" };
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Guest"}
+                        subtext="Access and manage you account and transactions efficiently"
+                    />
+                    <TotalBalanceBox accounts ={[]} totalBanks = {1} totalCurrentBalance ={1250.00}/>
+                </header>
+            </div>
+        </section>
+    );
 }
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import CountUp from "react-countup";
+
+export default function AnimatedCounter({ amount }: { amount: number }) {
+    return (
+        <div className="w-full">
+
+            <CountUp end={amount} duration={1} decimals={2} prefix="$" separator="," />
+        </div>
+    );
+}
